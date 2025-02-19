2c458fb8ff68409624c1ea99187bd14cfef77c59
afridi00000 <afridi@usf.edu>
2025-02-19 18:55:36 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -42,3 +42,22 @@ class Order {
 const order1 = new Order(501, prod1, 2);
 console.log(order1.getOrderDetails()); // Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
 console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
+// Task 3: Creating an Inventory Class
+class Inventory {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    listProducts() {
+        this.products.forEach(product => console.log(product.getDetails()));
+    }
+}
+
+const inventory = new Inventory();
+inventory.addProduct(prod1);
+inventory.listProducts();
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
